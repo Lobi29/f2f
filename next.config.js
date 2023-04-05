8f047896a166201e03d475b3d0c94c1121b2eb57
chao lobi <gogoiladitya@gmail.com>
2023-04-06 00:42:34 +0530

    main video view setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['photos.app.goo.gl']
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,43 @@
-import type { NextPage } from "next";
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[]
+}
+
+
+const Home = ({ videos }: IProps) => {
+
+  console.log(videos);
+
+  
   return (
-    <div className="text-3xl font-bold underline" style={{fontFamily: "cursive"}}>
+    <div className='flex flex-col gap-10
+     videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
       hellow
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/post');
+
+
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home;
